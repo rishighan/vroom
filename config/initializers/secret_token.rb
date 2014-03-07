dc98fe94b20aac5cdcfadccdf72e9cf82b61e527
Rishi Ghan <rishi.ghan@gmail.com>
2014-03-07 14:58:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rishighan::Application.config.secret_key_base = 'af8cc8f482fd809d244a6c8238ea39dbf7fd811173178c182eb3ae509811f0e33a664483ded28c2f447e351953379d5b643ace3e1ad752b461c9268ae53aabe8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rishighan::Application.config.session_store :cookie_store, key: '_rishighan_session'

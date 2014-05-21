4dcaac30ee854602ae04ae0623b74d102ad228f4
Rishi Ghan <rishi.ghan@gmail.com>
2014-05-21 10:53:15 -0400

    heroku test environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ RishighanCom::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'images', 'illustrations')
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false

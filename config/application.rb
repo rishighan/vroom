b26737775da8f7c4053439d5ad8124689016a03f
Rishi Ghan <rishi.ghan@gmail.com>
2014-05-22 10:42:41 -0400

    debugging issues with asset pipeline and fonts

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module RishighanCom
     config.action_controller.perform_caching = true
 
     config.assets.compile = false
+    config.assets.enable = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.

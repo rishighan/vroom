4895da31719457fef0805b4ec3714214973392d8
Rishi Ghan <rishi.ghan@gmail.com>
2014-06-10 18:11:41 -0400

    debugging the update action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,10 +31,17 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
 
         if @post.update(post_params)
-            redirect_to @posts
+            redirect_to @post
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
     end
 
     private
